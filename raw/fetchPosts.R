75e096319589245c706733231371a84a9869ba32
Dirk Eddelbuettel <edd@debian.org>
2026-07-21 09:33:20 -0500

    Updated with seventeen new ones, posts sorted by incr. 'created_at'

--- raw/fetchPosts.R
+++ raw/fetchPosts.R
@@ -7,8 +7,11 @@ suppressMessages({
 
 ## limit of 1500 is shooting over the top, we filter later
 sk <- atrrr::get_skeets_authored_by(actor = "kevinmkruse.bsky.social", parse = TRUE, limit=1500L)
-mm <- sk |> filter(startsWith(author_handle, "kevinmkruse")) |> filter(startsWith(text, "My man "))
-now <- format(Sys.time(), "%F-%R")
+mm <- sk |>
+    filter(startsWith(author_handle, "kevinmkruse")) |>
+    filter(startsWith(text, "My man ")) |>
+    arrange(created_at)
+now <- format(Sys.time(), "%F-%H-%M")
 saveRDS(mm, file=paste0("myman_", now, ".rds"))
 txt <- mm[, "text"]
 colnames(txt) <- "post"
