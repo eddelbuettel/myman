0f145f19ecbb8d395f23e5d8e20470c6f4fe2bb1
Dirk Eddelbuettel <edd@debian.org>
2026-08-13 05:21:54 -0500

    Added 39 new ones

--- raw/append.sh
+++ raw/append.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+newest=$(ls -1tr myman_*.csv | tail -n 1)
+#echo "Newest is ${newest}"
+
 # Use the full July set, and append the newest file modulo its header line
 # to form a new full csv with a header (from first file) and quoted entries
-cat myman_2026-07-25-13-56.csv <(sed -n '2,$p' myman_2026-08-12-12-24.csv)
+cat myman_2026-07-25-13-56.csv <(sed -n '2,$p' ${newest})
