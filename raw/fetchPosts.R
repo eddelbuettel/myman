0f1724262464797e4583284a4e0f7572b4f7a207
Dirk Eddelbuettel <edd@debian.org>
2026-08-31 06:53:42 -0500

    Ninety two new posts

--- raw/fetchPosts.R
+++ raw/fetchPosts.R
@@ -19,13 +19,18 @@ cheung$indexed_at <- as.POSIXct(cheung$indexed_at)
 cheung$created_at <- as.POSIXct(cheung$created_at)
 max_cheung <- max(cheung$created_at)
 
+kennedy <- readRDS("myman_2026-08-28-15-01.rds")
+kennedy$indexed_at <- as.POSIXct(kennedy$indexed_at)
+kennedy$created_at <- as.POSIXct(kennedy$created_at)
+max_kennedy <- max(kennedy$created_at)
+
 ## limit of 1500 is shooting over the top, we filter later
-sk <- atrrr::get_skeets_authored_by(actor = "kevinmkruse.bsky.social", parse = TRUE, limit=1500L)
+sk <- atrrr::get_skeets_authored_by(actor = "kevinmkruse.bsky.social", parse = TRUE, limit=500L)
 mm <- sk |>
     filter(startsWith(author_handle, "kevinmkruse")) |>
     filter(startsWith(text, "My man ")) |>
     arrange(created_at) |>
-    filter(created_at > max_cheung)
+    filter(created_at > max_kennedy)
 now <- format(Sys.time(), "%F-%H-%M")
 saveRDS(mm, file=paste0("myman_", now, ".rds"))
 txt <- mm[, "text"]
