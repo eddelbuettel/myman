05777c99a26d89d0cdd48a387050f4e8bde9808d
Dirk Eddelbuettel <edd@debian.org>
2026-09-12 17:06:39 -0500

    Initial (we assume there will be more) 57 JD Vance posts

--- raw/fetchPosts.R
+++ raw/fetchPosts.R
@@ -7,11 +7,11 @@ suppressMessages({
 
 setwd("~/git/myman/raw/")
 
-## we need the date of the last bessent post.
-bessent <- readRDS("myman_2026-08-31-22-09.rds")
-bessent$indexed_at <- as.POSIXct(bessent$indexed_at)
-bessent$created_at <- as.POSIXct(bessent$created_at)
-max_bessent <- max(bessent$created_at)
+## we need the date of the last mullin post.
+mullin <- readRDS("myman_2026-09-06-07-18.rds")
+mullin$indexed_at <- as.POSIXct(mullin$indexed_at)
+mullin$created_at <- as.POSIXct(mullin$created_at)
+max_mullin <- max(mullin$created_at)
 
 
 ## limit of 1500 is shooting over the top, we filter later
@@ -20,7 +20,7 @@ mm <- sk |>
     filter(startsWith(author_handle, "kevinmkruse")) |>
     filter(startsWith(text, "My man ")) |>
     arrange(created_at) |>
-    filter(created_at > max_bessent)
+    filter(created_at > max_mullin)
 mm |> select(created_at, text) |> head()
 mm |> select(created_at, text) |> tail()
 dim(mm)
