8245939caa9969517d8ca013ac5a9749db408447
Dirk Eddelbuettel <edd@debian.org>
2026-09-16 13:29:17 -0500

    Now 1594 posts

--- raw/blocks.R
+++ raw/blocks.R
@@ -55,10 +55,18 @@ range(vance$indexed_at)
 vance$man <- "JD Vance"
 dim(vance)
 
+hegseth <- readRDS("myman_2026-09-16-13-17.rds")
+hegseth$indexed_at <- as.POSIXct(hegseth$indexed_at)
+hegseth$created_at <- as.POSIXct(hegseth$created_at)
+range(hegseth$indexed_at)
+# [1] "2026-09-16 17:05:17.792 UTC" "2026-09-16 18:13:47.180 UTC"
+hegseth$man <- "Pete Hegseth"
+dim(hegseth)
 
 
 
-all <- rbind(miller, kennedy, cheung, lutnick, bessent, mullin, vance)
+
+all <- rbind(miller, kennedy, cheung, lutnick, bessent, mullin, vance, hegseth)
 dim(all)
 res <- data.frame(post = gsub("\\n", "", all$text),
                   created = all$created_at,
